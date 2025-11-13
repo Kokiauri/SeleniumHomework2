bf749ec430e356d99beb0e3c8e8489f54ccfa17a
Kokiauri <23202236@ibsu.edu.ge>
2025-11-13 14:21:04 +0400

    Final

--- Kokiauri_hw2/src/test/java/FormTests.java
+++ Kokiauri_hw2/src/test/java/FormTests.java
@@ -1,17 +1,21 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FormTests {
     WebDriver driver;
     WebDriverWait wait;
-    ChromeOptions options;
 
     @BeforeClass
     public void setup(){
@@ -56,10 +60,33 @@ public class FormTests {
         cityInput.sendKeys("Delhi");
         cityInput.sendKeys(Keys.ENTER);
 
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
         driver.findElement(By.xpath("//*[@id=\"submit\"]")).click();
+
+        List<WebElement> rows = driver.findElements(By.xpath("table[contains(@class, 'table-dark')]//tbody/tr"));
+
+        Map<String, String> expectedData = new HashMap<>();
+        expectedData.put("Student Name", "Bon Do");
+        expectedData.put("Student Email", "Bondo@Bon.Do");
+        expectedData.put("Gender", "Male");
+        expectedData.put("Mobile", "0123456789");
+        expectedData.put("Date of Birth", "31 October,2005");
+        expectedData.put("Subjects", "English");
+        expectedData.put("Hobbies", "Reading");
+        expectedData.put("Address", "Mojave Wasteland");
+        expectedData.put("State and City", "NCR Delhi");
+
+        for (WebElement row : rows) {
+            String label = row.findElement(By.xpath("./td[1]")).getText();
+            String value = row.findElement(By.xpath("./td[2]")).getText();
+            if (expectedData.containsKey(label)) {
+                Assert.assertEquals(value, expectedData.get(label), "mismatch: " + label);
+            }
+        }
+
     }
     @AfterClass
     public void tearDown(){
-    //driver.quit();
+    driver.quit();
     }
 }
